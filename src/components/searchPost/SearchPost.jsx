05614fa6dc5ca295b27785bf0244cee6626f08ed
Somnath Das <somnath8das@gmail.com>
2025-09-18 21:18:03 +0530

    search-blog

--- src/components/searchPost/SearchPost.jsx
+++ src/components/searchPost/SearchPost.jsx
@@ -4,7 +4,7 @@ import styles from './searchPost.module.css'
 export default function SearchPost() {
     return (
         <div className={styles.container}>
-            <div className={styles.title}>Search Post</div>
+            <div className={styles.title}>Search Posts</div>
             <div className={styles.searchContainer}>
                 <input type="text" placeholder="Latest news, tech etc..." className={styles.searchBox} />
                 <div className={styles.searchIcon}><Image src="/search.png" width={28} height={28} alt="logo" /></div>
