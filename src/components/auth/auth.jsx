9be86c5909ad449815abaed987ef15b4ad172107
Somnath Das <somnath8das@gmail.com>
2025-09-18 15:58:46 +0530

    nav-responsive

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
 import styles from "./auth.module.css"
+import { useState } from "react";
 
 export default function Auth({ theme }) {
+    const [open, setOpen] = useState(false);
     const status = "notauthenticated";
     return (
         <>{
@@ -13,6 +16,25 @@ export default function Auth({ theme }) {
                     <span className={theme === "dark" ? styles.darkLink : styles.link}>Logout</span>
                 </div>
             )
-        }</>
+        }   <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">About</Link>
+                    <Link href="/">Contact</Link>
+                    {status === "authenticated" ? (
+                        <Link href="/login" className={theme === "dark" ? styles.smallDarkLink : styles.smallLink}>Login</Link>
+                    ) : (
+                        <>
+                            <Link href="/write" className={theme === "dark" ? styles.smallDarkWriteBtn : styles.smallWriteBtn} >Write</Link>
+                            <span className={theme === "dark" ? styles.smallDarkLink : styles.smallLink}>Logout</span>
+                        </>
+                    )}
+                </div>
+            )}</>
     )
 }
