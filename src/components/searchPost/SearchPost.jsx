cecb2cfcffdef64743b650106e8182c792971e38
Somnath Das <somnath8das@gmail.com>
2025-09-19 09:02:22 +0530

    search-blog

--- src/components/searchPost/SearchPost.jsx
+++ src/components/searchPost/SearchPost.jsx
@@ -1,14 +1,25 @@
+"use client";
 import Image from 'next/image'
 import styles from './searchPost.module.css'
+import { useContext } from 'react';
+import { ThemeContext } from '@/context/ThemeContext';
 
 export default function SearchPost() {
+    const { toggle, theme } = useContext(ThemeContext);
     return (
         <div className={styles.container}>
-            <div className={styles.title}>Search Posts</div>
-            <div className={styles.searchContainer}>
-                <input type="text" placeholder="Latest news, tech etc..." className={styles.searchBox} />
-                <div className={styles.searchIcon}><Image src="/search.png" width={28} height={28} alt="logo" /></div>
-
+            <div className={theme === 'dark' ? styles.title_dark : styles.title}>Search Posts</div>
+            <div style={{ display: "flex", gap: "8px" }}>
+                <div className={styles.searchContainer}>
+                    <input type="text" placeholder="Latest news, tech etc..." className={theme === 'dark' ? styles.searchBoxDark : styles.searchBox} />
+                </div>
+                <div className={theme === 'dark' ? styles.searchIconDark : styles.searchIcon}>
+                    <Image
+                        src={"/search.png"}
+                        width={30}
+                        height={30}
+                        alt="logo"
+                    /></div>
             </div>
         </div>
     )
